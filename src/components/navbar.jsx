0a6b47648ccb0782d0c498efb38feb097bcf05de
ayush bansal <ayu023ban@gmail.com>
2020-07-26 14:23:39 +0530

    added basic structure and made folders

---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import {Menu} from 'semantic-ui-react'
+import { render } from '@testing-library/react'
+
+class  NavBar extends Component{
+    render(){
+        return (
+            <Menu>
+                <Menu.Item>
+                </Menu.Item>
+                <Menu.Item>
+                </Menu.Item>
+            </Menu>
+        )
+    }
+}
+export default NavBar
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'semantic-ui-css/semantic.min.css'
+import NavBar from './components/navbar';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App >
+      <NavBar/>
+    </App>
   </React.StrictMode>,
   document.getElementById('root')
 );
